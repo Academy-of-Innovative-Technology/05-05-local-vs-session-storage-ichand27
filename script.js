018462589ade05991b8342401d68729624244c04
ichand27 <ichand27@aoiths.org>
2026-06-09 16:16:58 -0400

    COMPLETE

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let submit = document.querySelector(".submit");
-let date = document.querySelector("#date");
-let riddle = document.querySelector("#riddle");
+let dateInput = document.querySelector("#date");
+let riddleInput = document.querySelector("#riddle");
 
 submit.addEventListener("click", function(e) {
   e.preventDefault();
-  
-    // localStorage here
-  localStorage.setItem(date, date.value);
-  
-  
-  // sessionStorage here
-  sessionStorage.setItem(riddle, riddle.value);
 
+  let dateValue = dateInput.value;
+  let riddleValue = riddleInput.value;
+
+  localStorage.setItem("local storage", dateValue);
+
+  sessionStorage.setItem("riddle", riddleValue);
+
+  console.log(sessionStorage.getItem("riddle"));
 });
-
-
-console.log(sessionStorage.getItem(riddle));
--- script2.js
+++ script2.js
@@ -1,4 +1,14 @@
-let ls = document.querySelector("h1");
-let ss = document.querySelector("h1");
+let button = document.querySelector(".printContent");
 
-let btn = docume
+let localOutput = document.querySelector("#localContent");
+let sessionOutput = document.querySelector("#sessionContent");
+
+button.addEventListener("click", function () {
+
+  let localData = localStorage.getItem("local storage");
+  let sessionData = sessionStorage.getItem("riddle");
+
+  localOutput.textContent = localData;
+  sessionOutput.textContent = sessionData;
+
+});
